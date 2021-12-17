5b102c610dcceb4b21150b3f12274e5087b6a212
邓卫平 <dengwp@smcaiot.com>
2021-12-17 19:26:20 +0800

    学生管理系统初始化

---
+++ src/utils/i18n.js
@@ -0,0 +1,20 @@
+export default {
+  'zh-cn': {
+    i18n: {
+      breadcrumb: '国际化',
+      tips: '通过切换语言按钮，来改变当前内容的语言。',
+      btn: '切换英文',
+      title: '学生工作管理系统',
+      p: '全方位实现学生管理工作的系统化、科学化、规范化和数据化。'
+    }
+  },
+  'en': {
+    i18n: {
+      breadcrumb: 'International',
+      tips: 'Click on the button to change the current language. ',
+      btn: 'Switch Chinese',
+      title: 'Student Management System',
+      p: 'Fully realize the systematization, scientification, standardization and dataization of student management.'
+    }
+  }
+}
---
+++ src/views/I18n.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class="main">
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item>
+                    <i class="el-icon-lx-global"></i>
+                    {{$t('i18n.breadcrumb')}}
+                </el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="container">
+            <span>{{$t('i18n.tips')}}</span>
+            <el-button
+                type="primary"
+                @click="$i18n.locale = $i18n.locale === 'zh-cn'?'en':'zh-cn';"
+            >{{$t('i18n.btn')}}</el-button>
+            <div class="list">
+                <h2>{{$t('i18n.title')}}</h2>
+                <p>{{$t('i18n.p')}}</p>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "i18n"
+};
+</script>
+
+<style scoped>
+.list {
+    padding: 30px 0;
+}
+.list p {
+    margin-bottom: 20px;
+}
+</style>
